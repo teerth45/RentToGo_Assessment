bcef8eac939ea2059e93335123e52af1b0e63fe5
Sihjie ONG <1813899@nzse.ac.nz>
2021-09-22 16:33:09 +1200

    I have added the Agent class

---
+++ RentToGo_Assessment/Models/Agent.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RentToGo_Assessment.Models
+{
+    class Agent
+    {
+        public int Id { get; set; }
+
+        public string AgentName { get; set; }
+
+        public string AgentEmail { get; set; }
+
+        public string AgentPhnNumber { get; set; }
+
+        public string AgentOffice { get; set; }
+    }
+}
